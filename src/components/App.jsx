b0e741bd8c9c57beaca84cfffd6b89c52ee9d8e3
Franklinshtein <98875248+Franklinshtein@users.noreply.github.com>
2023-06-17 18:42:21 +0200

    notification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import Statistics from '../components/Statistics/Statistics';
-import FeedbackOptions from '../components/FeedbackOptions/FeedbackOptions';
-import Section from '../components/Section/Section';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from '../components/Notification/Notification';
 
 const App = () => {
   const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
@@ -15,8 +16,7 @@ const App = () => {
 
   const { good, neutral, bad } = state;
   const totalFeedback = good + neutral + bad;
-  const positivePercentage = totalFeedback ? ((good / totalFeedback) * 100).toFixed(2) : 0;
-
+  const positivePercentage = totalFeedback ? (good / totalFeedback) * 100 : 0;
 
   return (
     <div>
@@ -27,13 +27,17 @@ const App = () => {
         />
       </Section>
       <Section title="Statistics">
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={totalFeedback}
-          positivePercentage={positivePercentage}
-        />
+        {totalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
       </Section>
     </div>
   );
